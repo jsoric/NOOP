b89e12a589a38795a2dc9b5d8d44aed58156f6e2
jsoric <josipsoric98@gmail.com>
2024-11-26 19:05:33 +0100

    .

---
+++ Java_Git01/SOLUTIONS/src/com/josipsoric/zadatak_1/FontPanelListener.java
@@ -0,0 +1,7 @@
+package com.josipsoric.zadatak_1;
+
+import java.util.EventListener;
+
+public interface FontPanelListener extends EventListener {
+    void fontSettings(String fontName, int fontSize);
+}
