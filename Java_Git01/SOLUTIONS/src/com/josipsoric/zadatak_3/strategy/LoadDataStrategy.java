f9becc3fd2919870bb4000e0c4a7a42dc5a08396
jsoric <josipsoric98@gmail.com>
2024-12-08 18:34:53 +0100

    .

--- Java_Git01/SOLUTIONS/src/com/josipsoric/zadatak_3/strategy/LoadDataStrategy.java
+++
@@ -1,7 +0,0 @@
-package com.josipsoric.zadatak_3.Strategy;
-
-import java.util.List;
-
-public interface LoadDataStrategy <E> {
-    List<E> loadDataFromFile();
-}
--- Java_Git01/SOLUTIONS/src/com/josipsoric/zadatak_3/strategy/SaveDataStrategy.java
+++
@@ -1,7 +0,0 @@
-package com.josipsoric.zadatak_3.Strategy;
-
-import java.util.List;
-
-public interface SaveDataStrategy<E> {
-    void saveDataToFile(List<E> data);
-}
